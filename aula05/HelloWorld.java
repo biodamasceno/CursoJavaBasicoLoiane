138c19aa85ffab18c69a7f9a4692677e99c28bb2
Fábio Damasceno <biodamasceno@gmail.com>
2017-04-19 10:57:42 -0300

    Aulas de Java
    Aulas de Java Básico com Loiane

---
+++ aula05/HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld{
+
+	public static void main(String[]args){
+
+	System.out.println("Ola Mundo");
+	}
+}
+
+//Fazer a compilação pelo terminal
+//javac  HelloWorld.java
+//
+//executar a classe já compilada
+//java HelloWorld 
---
+++ aula05/OlaMundo.java
@@ -0,0 +1,5 @@
+class OlaMundo{
+	public static void main(String[]args){
+		System.out.println("Meu teste da aula 05");
+	}
+}
